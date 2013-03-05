8c9aea3d81e1b8958099fbb396aaf7fe6b79ff71
Jesse Eaton <jesse.eaton@tufts.edu>
2013-03-05 00:56:41 -0500

    Made my own toRad

--- where/boner.js
+++ where/boner.js
@@ -293,11 +293,11 @@ function distance(lat1,lon1,lat2,lon2)
 
 	var R = 3958.756; // miles
 	var x1 = lat2-lat1;
-	var dLat = x1.toRad();  
+	var dLat = x1*Math.PI()/180;  
 	var x2 = lon2-lon1;
-	var dLon = x2.toRad(); 
-	var lat1 = lat1.toRad();
-	var lat2 = lat2.toRad();
+	var dLon = x2*Math.PI()/180; 
+	var lat1 = lat1*Math.PI()/180;
+	var lat2 = lat2*Math.PI()/180;
 
 	var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
         Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2); 
