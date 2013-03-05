72386c06974aa2f9e4bf6e98a80dd19b356452fe
Jesse Eaton <jesse.eaton@tufts.edu>
2013-03-05 00:55:01 -0500

    Using other code

--- where/boner.js
+++ where/boner.js
@@ -292,10 +292,10 @@ function distance(lat1,lon1,lat2,lon2)
 	// Copied from < http://www.movable-type.co.uk/scripts/latlong.html >
 
 	var R = 3958.756; // miles
-	console.log('lat2: ' + lat2 + ', lat1: ' + lat1);
-	var dLat = (lat2-lat1).toRad();
-	console.log('dLat: ' + dLat);
-	var dLon = (lon2-lon1).toRad();
+	var x1 = lat2-lat1;
+	var dLat = x1.toRad();  
+	var x2 = lon2-lon1;
+	var dLon = x2.toRad(); 
 	var lat1 = lat1.toRad();
 	var lat2 = lat2.toRad();
 
